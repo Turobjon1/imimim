468e8f4d2fdb56bff097065eaa72db71adea8793
Turobjon <avezovzamir337@gmail.com>
2024-04-14 07:39:02 +0500

    salom

---
+++ script.js
@@ -0,0 +1,7 @@
+let randomNum = Math.round(Math.random()*100)
+
+alert(randomNum)
+
+alert(randomNum + ` ikkkiga kopaytirsa ` + randomNum*2 + ` ga teng `  )
+
+alert(randomNum + ` ikkkiga bolsa ` + randomNum/2 + ` ga teng `  )
